2fd0ffd06ca5ab76631285a4e96ee1aabdcb97d9
Lizz Armstrong <lizzarmstrong@gmail.com>
2014-04-14 17:38:09 -0400

    First commit - blank rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mentorminder::Application.config.secret_key_base = 'ebc718c94c1aa9c958ad63074fb55e1a89b3775bcab3707144c8073d80822c9bd6c3c85118b09afa4c899e79b2813e1af77ad315f21ded99f282995eb209e34d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mentorminder::Application.config.session_store :cookie_store, key: '_mentorminder_session'
