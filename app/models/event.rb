2084e953590e57f59a09fd7bc311ffe55d90996f
Lizz Armstrong <lizzarmstrong@gmail.com>
2014-04-22 20:50:37 -0400

    Added relationships between Events and Mentors through SignUps

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ActiveRecord::Base
+	has_many :sign_ups
 	has_many :mentors, through: :sign_ups
 end
--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,3 +1,4 @@
 class Mentor < ActiveRecord::Base
+	has_many :sign_ups
 	has_many :events, through: :sign_ups
 end
