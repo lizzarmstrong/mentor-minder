911288c08a7368b0c5b3814212fb112d0fcc1deb
Lizz Armstrong <lizzarmstrong@gmail.com>
2014-04-28 21:30:40 -0400

    Dashboard basics

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ActiveRecord::Base
 	has_many :sign_ups
 	has_many :mentors, through: :sign_ups
+
+	def self.upcoming
+		where(['event_date > ?', Date.today])
+	end
 end
--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,4 +1,8 @@
 class Mentor < ActiveRecord::Base
 	has_many :sign_ups
 	has_many :events, through: :sign_ups
+
+	def self.gendered (x)
+		where(gender: x)
+	end
 end
