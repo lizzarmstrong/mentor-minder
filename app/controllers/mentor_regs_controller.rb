87b2c469e4c28b4a3fbfcbfcab436dc0769af65f
Lizz Armstrong <lizzarmstrong@gmail.com>
2014-05-22 20:19:22 -0400

    Public application form is working and you can delete pending apps

--- app/controllers/mentor_regs_controller.rb
+++ app/controllers/mentor_regs_controller.rb
@@ -1,4 +1,7 @@
 class MentorRegsController < ApplicationController
+	skip_before_action :authenticate_user!, only: [:new, :create, :thanks]
+	before_action :set_mentor_reg, only: [:destroy]
+
 	def index
 		@mentor_regs = MentorReg.all
 	end	
@@ -13,6 +16,53 @@ class MentorRegsController < ApplicationController
 		redirect_to mentor_regs_path
 	end
 
+	def new
+		@mentor_reg = MentorReg.new
+	end
+
+	def create
+		if MentorReg.create(mentor_reg_params)
+			redirect_to thanks_mentor_regs_path, notice: "Application was successful"
+		else
+			redirect_to thanks_mentor_regs_path, notice: "Application Failed"
+		end
+	end
+
+	def thanks
+
+	end
+
+	def destroy
+		@mentor_reg.destroy
+		redirect_to mentor_regs_path
+	end
+
+	private
+
+	def set_mentor_reg
+		@mentor_reg = MentorReg.find(params[:id])
+	end
+	def mentor_reg_params
+		params.require(:mentor_reg).permit(
+					:first_name,
+					:last_name,
+					:email,
+					:gender,
+					:first_aid_certified,
+					:phone,
+					:city,
+					:website,
+					:twitter,
+					:linkedin,
+					:work,
+					:job,
+					:bio,
+					:why_involved,
+					:emergency_contact,
+					:references,
+					:permission_to_contact_references
+					)
+	end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Mentorminder::Application.routes.draw do
     member do
       post :process_reg
     end
+    collection do
+      get :thanks
+    end
   end
 
   #post 'mentor_regs/:id', to: 'mentor_regs#process_reg' 
