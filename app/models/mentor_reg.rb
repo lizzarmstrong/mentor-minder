9c035e6222bda0ceacfa51edbd7c9aa5d4f99cf6
Lizz Armstrong <lizzarmstrong@gmail.com>
2014-05-26 15:31:40 -0400

    Added mentor_reg creation validation and some cosmetic changes

--- app/models/mentor_reg.rb
+++ app/models/mentor_reg.rb
@@ -2,6 +2,8 @@ class MentorReg < ActiveRecord::Base
 	has_many :sign_ups, as: :signupable
 	has_many :events, through: :sign_ups
 
+	validates :first_name, :last_name, :email, presence: true
+
 	def self.pending?
 		where(pending: true)
 	end
